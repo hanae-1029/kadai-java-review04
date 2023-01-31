8111645d1a2a35d89892ad1c67b4f8a9178ea180
hanae-1029 <nichibi1029@icloud.com>
2023-01-31 10:05:10 +0900

    Javaプログラミング入門学習終了

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("disny");
+        System.out.println("usj");
+        System.out.println("行きたい！");   
+    }
+
+}
