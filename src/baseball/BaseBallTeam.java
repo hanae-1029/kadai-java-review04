6aeb4ea0d916a8ee9879ad24805b6f4bc715e17c
hanae-1029 <nichibi1029@icloud.com>
2023-02-08 17:51:53 +0900

    課題

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,55 @@
+package baseball;
+
+public class BaseBallTeam {
+
+    String name;
+    int win, lose, draw;
+
+    BaseBallTeam(String name, int win, int lose, int draw) {
+        setName(name);
+        setWin(win);
+        setLose(lose);
+        setDraw(draw);
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getWin() {
+        return win;
+    }
+
+    void setWin(int win) {
+        this.win = win;
+    }
+
+    int getLose() {
+        return lose;
+    }
+
+    void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    int getDraw() {
+        return draw;
+    }
+
+    void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    double getRate() {
+        return (double) getWin() / (getWin() + getLose());
+    }
+
+    void report() {
+        System.out.printf("%s の2022年の成績は %d勝 %d敗 %d分、勝率は %fです。%n", getName(), getWin(), getLose(), getDraw(),
+                getRate());
+    }
+}
